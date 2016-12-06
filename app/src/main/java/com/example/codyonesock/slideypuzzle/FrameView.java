96f0f534ce668b9fb10ad2ee9216a85ee66ed8f8
Cody Denny <cody_denny@hotmail.com>
2016-12-06 00:06:29 -0400

    I'm dead with handling the frame functionality so i'm moving to ui

---
+++ app/src/main/java/com/example/codyonesock/slideypuzzle/FrameView.java
@@ -0,0 +1,12 @@
+package com.example.codyonesock.slideypuzzle;
+
+/* This class is the view where I'm going to generate the graphics for scalable buttons
+ * the reason for this is to handle different difficulty sizes without having an excessive
+ * amount of views.
+ */
+
+public class FrameView {
+
+    
+
+}
