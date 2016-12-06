4a592eb3a532a0764d379e1a75ccb14c781f7688
Cody Denny <cody_denny@hotmail.com>
2016-12-06 02:32:35 -0400

    lol I broke everything

--- app/src/main/java/com/example/codyonesock/slideypuzzle/Frame.java
+++ app/src/main/java/com/example/codyonesock/slideypuzzle/Frame.java
@@ -4,18 +4,53 @@ package com.example.codyonesock.slideypuzzle;
  * can be placed based off the coordinates.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frame {
     //variables
     private final int size;
 
+    //all the potential places for the frame
+    private final List<BlockPosition> blockPositions;
+
+    //find all potential future frame changes and new coordinates
+    private final List<PotentialPositions> potentialPositions;
+
     //Creating a new Frame and giving it's dimensions of the initial puzzle from the start
     public Frame(int size) {
+        potentialPositions = new ArrayList<PotentialPositions>();
         this.size = size;
+        blockPositions = new ArrayList<BlockPosition>(size * size);
+        for (int x = 1; x <= size; x++) {
+            for (int y = 1; y <= size; y++) {
+                blockPositions.add(x == size && y == size ? new BlockPosition(x, y, this) : new BlockPosition(x, y, (y - 1) * size + x, this));
+            }
+        }
     }
 
+
     public boolean canMove(BlockPosition blockPosition) {
+        return true;
     }
 
     public void move(Block block) {
+    }
+
+    //---------------------------get/set
+    public int getSize() {
+        return size;
+    }
+
+    public Iterable<BlockPosition> blockPositions() {
+        return blockPositions;
+    }
+
+    //-----------------------------------------------------------listeners
+
+    //this listens to all states of potential changes
+    public interface PotentialPositions {
+        //when a block is moved 'FROM' a spot "TO" and empty spot both change states
+        void blockMoved(BlockPosition from, BlockPosition to);
     }
 }
--- app/src/main/java/com/example/codyonesock/slideypuzzle/FrameView.java
+++ app/src/main/java/com/example/codyonesock/slideypuzzle/FrameView.java
@@ -6,14 +6,19 @@ package com.example.codyonesock.slideypuzzle;
  */
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.graphics.Color;
+
+import java.util.Iterator;
 
 public class FrameView extends View {
     //access to the frame
     private Frame frame;
+    private Context context;
 
     //my canvas objects
     Paint background;
@@ -37,8 +42,69 @@ public class FrameView extends View {
     //-------------------------------------------------Button Image Generation
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint background = new Paint();
-        background.setColor(getResources().getColor(R.color.colorAccent, ));
+        //drawing the rectangle for each button
+        Paint button = new Paint();
+        button.setColor(getResources().getColor(R.color.buttonColor));
+        canvas.drawRect(0, 0, getWidth(), getHeight(), button);
 
+        //drawing the background and adding a stroke line to split buttons a bit
+        Paint background = new Paint();
+        background.setColor(getResources().getColor(R.color.backgroundColor));
+        background.setStrokeWidth(15);
+
+        //drawing the grid lines for the background based off the size of the frame
+        for (int i = 0; i < this.frame.getSize(); i++) {
+            canvas.drawLine(0, i * height, getWidth(), i * height, background);
+            canvas.drawLine(i * width, 0, i * width, getHeight(), background);
+        }
+
+        //drawing and setting the number properties for the buttons
+        Paint numbers = new Paint(Paint.ANTI_ALIAS_FLAG);
+        numbers.setColor(getResources().getColor(R.color.numberColor));
+        numbers.setStyle(Paint.Style.FILL);
+        numbers.setTextSize(height * 0.75f);
+        numbers.setTextScaleX(width / height);
+        numbers.setTextAlign(Paint.Align.CENTER);
+
+        float x = width / 2;
+        Paint.FontMetrics fm = numbers.getFontMetrics();
+        float y = (height / 2) - (fm.ascent + fm.descent) / 2;
+
+        Iterator<BlockPosition> iterator = frame.blockPositions().iterator();
+        for (int i = 0; i < frame.getSize(); i++) {
+            for (int k = 0; k < frame.getSize(); k++) {
+                if (iterator.hasNext()) {
+                    BlockPosition blockPosition = iterator.next();
+                    if (blockPosition.hasBlock()) {
+                        String number = Integer.toString(blockPosition.getBlock().getId());
+                        canvas.drawText(number, i * width + x, k * height + y, numbers);
+                    } else {
+                        canvas.drawRect(i * width, k * height, i * width + width, k * height + height, background);
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
