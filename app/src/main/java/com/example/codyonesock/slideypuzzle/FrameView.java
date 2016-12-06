0a99112dcaa562446da9f5f8a72994f594d2bbe4
Cody Denny <cody_denny@hotmail.com>
2016-12-06 00:37:31 -0400

    frustrated drawing buttons going for food

--- app/src/main/java/com/example/codyonesock/slideypuzzle/FrameView.java
+++ app/src/main/java/com/example/codyonesock/slideypuzzle/FrameView.java
@@ -5,8 +5,40 @@ package com.example.codyonesock.slideypuzzle;
  * amount of views.
  */
 
-public class FrameView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
 
-    
+public class FrameView extends View {
+    //access to the frame
+    private Frame frame;
 
+    //my canvas objects
+    Paint background;
+
+    //I'm going to draw with simple W * H
+    private float width;
+    private float height;
+
+    //set new instance of my FrameView and give focus
+    public FrameView(Context context, Frame frame) {
+        super(context);
+        this.frame = frame;
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+    }
+
+    public void canvasInit() {
+        background = new Paint();
+    }
+
+    //-------------------------------------------------Button Image Generation
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint background = new Paint();
+        background.setColor(getResources().getColor(R.color.colorAccent, ));
+
+    }
 }
